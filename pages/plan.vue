7b6dbda25c71b1dd95129e53f063679b4483d5f7
Kendall Jackson <kendallkj00@gmail.com>
2025-04-04 23:39:14 -0400

    fix: better copy

--- pages/plan.vue
+++ pages/plan.vue
@@ -2,7 +2,7 @@
   <div class="py-10">
     <header>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900 dark:text-white">Custom Planning</h1>
+        <h1 class="text-3xl font-bold leading-tight text-gray-900 dark:text-white">Create a Crawl in Minutes.</h1>
       </div>
     </header>
     <main>
